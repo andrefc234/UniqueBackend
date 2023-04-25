3e9d12156f7c85b5ef7b2f43738ec205523e91c4
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2023-04-24 19:16:21 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,28 @@
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 
-const connectDB = async () => {
-  const conn = await mongoose.connect(`${process.env.MONGO_URI}`)
+const url =
+  'mongodb+srv://uniquelivingqro1:<P94YE2e2HwjDlYlU>@uniqueliving.c3fx2cg.mongodb.net/?retryWrites=true&w=majority'; 
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+const dbName = 'UniqueLiving'; 
+
+const client = new MongoClient(url);
+
+function connect(callback) {
+  client.connect(function (err) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    console.log('Connected successfully to server');
+
+    const db = client.db(dbName);
+
+    callback(null, db);
+  });
+}
+
+module.exports = {
+  connect: connect,
 };
 
-module.exports = connectDB;
