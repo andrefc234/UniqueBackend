047e0b9bcc8aa2145d3c16ec26c938a51ed2c896
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2023-05-19 13:39:29 -0600

    Update obra.js

--- controller/obra.js
+++ controller/obra.js
@@ -8,24 +8,53 @@ exports.getObras = async (req, res) => {
     res.status(500).json({ success: false, error: err.message });
   }
 };
+
 exports.getObra = async (req, res) => {
-    try {
-      const obra = await Obra.findById(req.params.id);
-      if (!obra) {
-        return res.status(404).json({ success: false, error: 'Obra not found' });
-      }
-      res.status(200).json({ success: true, data: obra });
-    } catch (err) {
-      res.status(500).json({ success: false, error: err.message });
+  try {
+    const obra = await Obra.findById(req.params.id);
+    if (!obra) {
+      return res.status(404).json({ success: false, error: 'Obra not found' });
     }
-  };
+    res.status(200).json({ success: true, data: obra });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
 exports.createObra = async (req, res) => {
-  console.log(req.body)
-  const obra = await Obra.create(req.body);
-  res.status(201).json({ success: true, data: obra });
- 
-  };
- 
+  try {
+    const obra = await Obra.create(req.body);
+    res.status(201).json({ success: true, data: obra });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+exports.updateObra = async (req, res) => {
+  try {
+    const obra = await Obra.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!obra) {
+      return res.status(404).json({ success: false, error: 'Obra not found' });
+    }
+    res.status(200).json({ success: true, data: obra });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+exports.deleteObra = async (req, res) => {
+  try {
+    const obra = await Obra.findByIdAndRemove(req.params.id);
+    if (!obra) {
+      return res.status(404).json({ success: false, error: 'Obra not found' });
+    }
+    res.status(200).json({ success: true, data: obra });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
 
 exports.addIdRequerimiento = async (req, res) => {
   const obraId = req.params.id;
