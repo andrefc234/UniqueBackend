9654d91d8bb183c0b4931ae99ffb17ca6b0d73a6
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2023-05-19 13:41:39 -0600

    Update obra.js

--- routes/obra.js
+++ routes/obra.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const router = express.Router()
-const {createObra,getObra,getObras,addIdRequerimiento} = require('../controller/obra')
+const express = require('express');
+const router = express.Router();
+const {
+  createObra,
+  getObra,
+  getObras,
+  updateObra,
+  deleteObra,
+  addIdRequerimiento,
+} = require('../controller/obra');
 
+router.get('/', getObras);
+router.get('/:id', getObra);
+router.post('/', createObra);
+router.put('/:id', updateObra);
+router.delete('/:id', deleteObra);
+router.put('/requerimientos/:id', addIdRequerimiento);
 
-router.get('/',getObras)
-router.get('/:id',getObra)
-router.post('/crear',createObra)
-router.put('/requerimientos/:id',addIdRequerimiento)
-module.exports = router
-
-
+module.exports = router;
