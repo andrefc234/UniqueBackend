ec48ec557a16fd79cdf16c0634839e0295b69225
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2023-05-21 17:07:50 -0600

    added new routes of recibo

--- routes/recibo.js
+++ routes/recibo.js
@@ -10,7 +10,9 @@ const {
   getRecibosNominaPersona,
   getReciboNominaPersonaById,
   updateReciboNominaPersona,
-  deleteReciboNominaPersona
+  deleteReciboNominaPersona,
+  getRecibosNominaObraByDate, // Added route for getting reciboNominaObra by date
+  getRecibosNominaPersonaByDate // Added route for getting reciboNominaPersona by date
 } = require('../controller/recibo');
 
 // Routes for reciboNominaObra
@@ -19,6 +21,7 @@ router.get('/obra', getRecibosNominaObra);
 router.get('/obra/:id', getReciboNominaObraById);
 router.put('/obra/:id', updateReciboNominaObra);
 router.delete('/obra/:id', deleteReciboNominaObra);
+router.get('/obra/date', getRecibosNominaObraByDate); // Route for getting reciboNominaObra by date
 
 // Routes for reciboNominaPersona
 router.post('/persona', createReciboNominaPersona);
@@ -26,5 +29,6 @@ router.get('/persona', getRecibosNominaPersona);
 router.get('/persona/:id', getReciboNominaPersonaById);
 router.put('/persona/:id', updateReciboNominaPersona);
 router.delete('/persona/:id', deleteReciboNominaPersona);
+router.get('/persona/date', getRecibosNominaPersonaByDate); // Route for getting reciboNominaPersona by date
 
 module.exports = router;
