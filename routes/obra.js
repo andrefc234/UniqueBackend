e4c7c70b12eda5abdb9cf6c03676b40f90c6625a
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2023-09-08 13:28:32 -0600

    change route

--- routes/obra.js
+++ routes/obra.js
@@ -16,5 +16,5 @@ router.post('/', createObra);
 router.put('/:id', updateObra);
 router.delete('/:id', deleteObra);
 router.put('/requerimientos/:id', addIdRequerimiento);
-router.post('/materialesPendientes/:id', updateMaterialesPendientes);
+router.put('/materialesPendientes/:id', updateMaterialesPendientes);
 module.exports = router;
