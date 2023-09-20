f526a979ecae73e7bd5c98f1173a40b977500c2c
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2023-09-20 16:48:13 -0600

    entregados

--- controller/obra.js
+++ controller/obra.js
@@ -79,27 +79,32 @@ exports.updateMaterialesAprobados = (req, res) => {
   });
 };
 
-exports.updateMaterialesEntregados = async (req, res) => {
+exports.updateMaterialesEntregados = (req, res) => {
   const { id } = req.params;
   const { materialesEntregados } = req.body;
-  
-  try {
-    const obra = await Obra.findByIdAndUpdate(
-      id,
-      { $push: { materialesEntregados: { $each: materialesEntregados } } }, // Use $push and $each to append new materials
-      { new: true }
-    );
+
+  Obra.findById(id, (err, obra) => {
+    if (err) {
+      return res.status(500).json({ success: false, message: 'Error finding obra', error: err });
+    }
 
     if (!obra) {
       return res.status(404).json({ success: false, message: 'Obra not found' });
     }
 
-    return res.status(200).json({ success: true, message: 'Materiales entregados updated successfully', obra });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: 'Error updating materiales entregados', error });
-  }
+    // Use the $push operator to add new materials to the existing array
+    obra.materialesEntregados.push(...materialesEntregados);
+
+    obra.save((err) => {
+      if (err) {
+        return res.status(500).json({ success: false, message: 'Error updating materiales entregados', error: err });
+      }
+
+      return res.status(200).json({ success: true, message: 'Materiales entregados updated successfully' });
+    });
+  });
 };
+
 
 exports.removeMaterialesAprobados = async (req, res) => {
   const { id, materialAprobadoId } = req.params;
