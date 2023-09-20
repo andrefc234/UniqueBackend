ad5528f279bafbe94f595a4a7e2b33049ac63a22
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2023-09-20 14:39:56 -0600

    change

--- models/requisicion.js
+++ models/requisicion.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose')
 
-
-
 const requisicion = new mongoose.Schema({
   obraId: {type:String},
   material:{type:[{
-    concepto:String,//obra
-    unidad:String,//obra
-    pu:Number,
-    cantidad:Number,//obra
-    importe:Number,
-    Observacion:String//compras y obra
+    Concepto:String,//obra
+    Unidad:String,//obra
+    Cantidad:Number,//obra
   }]},
   aceptado:{type:Boolean},
-  
+  fecha:{type:String}
 });
 
 
